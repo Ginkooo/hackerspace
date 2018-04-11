49b2ec1781aac062cf6f2d85eafdc2b83c11050b
Piotr Czajka <czajka@protonmail.com>
2018-04-11 17:26:25 +0200

    Implement intercom opening function, it sends udp packet to intercom doors driver, two new associated config values

--- src/intercom/intercom_handler.py
+++ src/intercom/intercom_handler.py
@@ -1,6 +1,12 @@
+import socket
+
+import config
+
+
 class IntercomHandler:
     """Handle intercom door opening"""
 
     def open():
-        """open intercom door, closing should be handled by external MPU"""
-        print('opened')
+        """Send any UDP packet to intercom driver to open the door"""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(b'data-is-irrelevant', (config.INTERCOM_DRIVER_IP, config.INTERCOM_DRIVER_PORT))
